18ce62e7d6a596db5565fe1039f91b15f94553b0
Ayodeji Oladimeji <66247222+Ayodejioladimeji@users.noreply.github.com>
2020-11-29 15:13:01 -0800

    netlify.toml adjusted

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./Nav.css";
 import "./NavQuery.css";
 import icon from "./layomi.png";
