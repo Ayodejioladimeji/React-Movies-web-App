4975e91340e6d00b53e841c1c9524b4f24090155
Ayodeji Oladimeji <66247222+Ayodejioladimeji@users.noreply.github.com>
2020-11-29 14:45:24 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Home from './components/Home/Home'
-import Movies from './components/Movies/Movie'
-import Crypto from './components/Crypto/Crypto'
-import Nav from './components/Nav/Nav'
-import './App.css';
+import Movies from "./components/Movies/Movie";
+import Nav from "./components/Nav/Nav";
+import "./App.css";
+
+import roll from "./Bean.gif";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=356c51928b82e523a9a48f9eb08e771b&page=2";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=356c51928b82e523a9a48f9eb08e771b&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="loading">
+        <img src={roll} alt="roll" />
+      </section>
+    );
+  }
+
+  // THE SEARCH FUNCTION
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+          if (data.length === 0) {
+            return <p>Not found</p>;
+          }
+        });
+      setSearchTerm("");
+    }
+  };
+
+  // THE HANDLE CHANGE FUNCTION
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-      <Router>
-        <Nav/>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/movies" exact component={Movies}/>
-          <Route path="/crypto" exact component={Crypto}/>
-        </Switch>
-      </Router>
+    <Router>
+      <Nav
+        searchTerm={searchTerm}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <Movies movies={movies} />
+    </Router>
   );
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,51 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = () => {
     return (
         <div className="contain">
-            <Container fluid>
-                <Row>
-                    <Col lg={12} style={{height:'500px'}}>
-                        <Carousel style={{margin:'0!important'}}>
-                            <Carousel.Item className={{width:'100%', height:'100%'}}>   
-                                <Row>
-                                    <Col lg={6} style={{height:'100vh',background:'red'}}>
-                                        <img src="./layomi.png" alt="left-ima"/>
-                                        
-                                    </Col>
-
-                                    <Col lg={6} style={{height:'100vh',background:'red'}}>
-                                    <p>coming</p>
-                                        
-                                    </Col>
-                                </Row>                               
-                            </Carousel.Item>
-
-                            <Carousel.Item className={{width:'100%', height:'100%'}}>   
-                                <Row>
-                                    <Col lg={6} style={{height:'100vh',background:'orange'}}>
-                                        home made
-                                    </Col>
-
-                                    <Col lg={6} style={{height:'100vh',background:'orange'}}>
-                                        home made
-                                    </Col>
-                                </Row>                               
-                            </Carousel.Item>
-
-                            <Carousel.Item className={{width:'100%', height:'100%'}}>   
-                                <Row>
-                                    <Col lg={6} style={{height:'100vh',background:'blue'}}>
-                                        home made
-                                    </Col>
-
-                                    <Col lg={6} style={{height:'100vh',background:'blue'}}>
-                                        home made
-                                    </Col>
-                                </Row>                               
-                            </Carousel.Item>
-                        </Carousel>
-                    </Col>
-                </Row>
-            </Container>
+            
         </div>
     )
 }
